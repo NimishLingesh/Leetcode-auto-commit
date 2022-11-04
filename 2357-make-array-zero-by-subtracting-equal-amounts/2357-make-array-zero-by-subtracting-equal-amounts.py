873fcbc5bab7449c7f0439dfec92c65dfffe1f8d
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2022-11-04 16:39:46 -0700

    Time: 29 ms (99.40%), Space: 13.9 MB (12.58%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,6 +1,11 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        count = 0
+        if 0 in nums:
+            return len(set(nums))-1
+        else:
+            return len(set(nums))
+        
+        """count = 0
         while(0 in nums):
             nums.remove(0)
         while(len(nums)>0):
@@ -12,6 +17,5 @@ class Solution:
                 if nums[idx] <= 0:
                     lst.insert(0, idx)
             for index in lst:
-                # print(index)
                 nums.pop(index)
-        return count
+        return count"""
